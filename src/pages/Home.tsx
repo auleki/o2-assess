0e67a320bb0fbbdfffca26d51d8fde602b3d7de5
auleki <kodagiwa@gmail.com>
2022-01-19 18:54:51 +0100

    navbar made responsive..

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { Header, Layout, Reviews } from '../layout'
 import FeaturedBikes from '../layout/FeaturedBikes'
 import BikeTypes from '../layout/BikeTypes'
 import TopBike from '../layout/TopBike'
-import ScrollToTop from '../components/ScrollToTop'
+// import ScrollToTop from '../components/ScrollToTop'
 
 const Home = () => {
   return (
